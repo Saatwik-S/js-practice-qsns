529c3d4bbde93cbdf661fd06029b58d905cd4dfc
Saatwik-S <saatwik_s@mckinsey.com>
2023-01-15 12:46:15 +0530

    feat: Concat multiple strings

---
+++ concatStrings.js
@@ -0,0 +1,14 @@
+/**
+ * 
+ * @param {Array<String>} arr
+ */
+const concatStringsArrFn = (arr) => arr.join('').substring(1);
+
+/**
+ * 
+ * @param {Array<String>} arr
+ */
+function concatStrings(arr) {return arr.join('').substring(1);}
+
+console.log(concatStrings(["code", "academy"]))
+
