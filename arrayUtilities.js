15cd426a279f845f8f0981daf69a3cd23eaa18b2
Saatwik-S <saatwik_s@mckinsey.com>
2023-01-16 21:20:34 +0530

    feat: Create ArrayUtils.js

--- arrayUtilities.js
+++ arrayUtilities.js
@@ -1,12 +1,47 @@
 
 const doubleNumbers = (numbers) => {
-    if (!Array.isArray(numbers) || numbers.length == 0){
-        throw ('Unsupported Type'); }
+    if (!Array.isArray(numbers)) throw 'Unsupported Type';
+    if (numbers.length == 0) throw 'Length of array is 0';
 
 
     return numbers.map(e => e * 2);
 
 };
 
-module.exports = { doubleNumbers };
+const tripleNumbers = (numbers) => {
+    if (!Array.isArray(numbers)) throw 'Unsupported Type';
+    if (numbers.length == 0) throw 'Length of array is 0';
+    return numbers.map(e => e * 3);
+
+}
+
+const filterArrayForEvenNumbers = (numbers) => {
+    if (!Array.isArray(numbers)) throw 'Unsupported Type';
+    if (numbers.length == 0) throw 'Length of array is 0';
+    const res = numbers.filter(e => e % 2 == 0);
+    return res.length == 0 ? -1 : res;
+}
+
+const tripleElementsAndfilterForEvenNumbers = (numbers) => {
+    if (!Array.isArray(numbers)) throw 'Unsupported Type';
+    if (numbers.length == 0) throw 'Length of array is 0';
+    const res = numbers.map(e => e * 3).filter(ele => ele % 2 == 0);
+    return res.length == 0 ? -1 : res;
+}
+
+const tripleElementsAndFilterNumbersUsingReduce = (numbers) => {
+
+    if (!Array.isArray(numbers)) throw 'Unsupported Type';
+    if (numbers.length == 0) throw 'Length of array is 0';
+    const res = [];
+    numbers.reducex ((previous, current) => {
+        if (current % 2 == 0) {
+            res.push(current * 3);
+        }
+
+    });
+    return res;
+}
+
+module.exports = { doubleNumbers, tripleElementsAndFilterNumbersUsingReduce, tripleElementsAndfilterForEvenNumbers, tripleNumbers };
 
---
+++ test.js
@@ -0,0 +1,5 @@
+const num = [1,2,3];
+num.reduce((prev, curr) => {
+    console.log(prev);
+    console.log(curr);
+})
