23349593e8f5b6a83370f5653f1d56f122256c33
Saatwik-S <saatwik_s@mckinsey.com>
2023-01-16 11:14:26 +0530

    fix: Add capital vowels as well

--- vowelCount.js
+++ vowelCount.js
@@ -1,6 +1,6 @@
 const { moduleExpression } = require("@babel/types");
 
-const vowels = ['a', 'e', 'i', 'o', 'u'];
+const vowels = ['a', 'e', 'i', 'o', 'u', 'A', 'E', 'I', 'O', 'U'];
 const countVowelsArrFn = (str) => {
     let counter = 0;
     str.split('').forEach(e => {
